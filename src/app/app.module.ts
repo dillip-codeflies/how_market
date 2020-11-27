cfc115040b4021c99e7807522f77f13b6b319822
Anurag <anuragtyagi014@gmail.com>
2020-11-27 19:03:02 +0530

    nevbar arun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HTTP } from '@ionic-native/http/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+username : string;
+password : string;
+  constructor(private http: HTTP) {
+this.username="";
+this.password="";
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
+login(){
+  this.http.get('https://wp.localserverweb.com/how-markets/how-api/auth/generate_auth_cookie/?username=john&password=PASSWORD-HERE&seconds=60', {}, {})
+  .then(data => {
+    console.log("working");
+    console.log(data.status);
+    console.log(data.data); // data received by server
+    console.log(data.headers);
 
+  })
+  .catch(error => {
+
+    console.log(error.status);
+    console.log(error.error); // error message as string
+    console.log(error.headers);
+
+  });
+
+
+
+}
 }
